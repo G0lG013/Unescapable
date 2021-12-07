7862e56aee776fcece17a2aaa4330d0e79242a55
Jerikooo <jericho321@hotmail.com>
2021-12-06 23:03:55 -0500

    Timer attempt

--- Unescapable 0.1/StageEight.java
+++ Unescapable 0.1/StageEight.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class StageEight extends ZombieWorld
 {
-
+    public int timer;
+    //public Part part = new Part();
     /**
      * Constructor for objects of class StageEight.
      * 
@@ -16,5 +17,22 @@ public class StageEight extends ZombieWorld
     public StageEight(int x, int y)
     {
         soldier.setLocation(x, y);
+        timer = 2000;
     }
+    public void act() {
+        //countdown();
+        showTimer();
+        changeHp();
+    }
+    public int getTimer() {
+        return timer;
+    }
+    public void showTimer() {
+        showText("Timer: " + getTimer(), 800, 600);
+    }
+    //public void countDown() {
+        //if (part.getAmountofParts() == 2) {
+            //timer--;
+        //}
+    //}
 }
